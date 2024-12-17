a73af3ee0ddd9fd63e8ba2f0b826ecb760bef5a8
WCY-dt <834421194@qq.com>
2024-12-18 01:03:06 +0800

    fix: Delete gunicorn

--- script/setup.sh
+++ script/setup.sh
@@ -9,7 +9,7 @@ if [[ "$response" != "y" && "$response" != "Y" ]]; then
 fi
 
 # Update package list and install necessary packages
-apt update && apt install -y python3.12 python3-pip nginx certbot python3-certbot-nginx python3-gunicorn python3-virtualenv || { echo "Failed to install packages"; exit 1; }
+apt update && apt install -y python3.12 python3-pip nginx certbot python3-certbot-nginx python3-virtualenv || { echo "Failed to install packages"; exit 1; }
 
 # Create and navigate to the web directory
 mkdir -p /var/www || { echo "Failed to create directory"; exit 1; }
@@ -30,13 +30,6 @@ sed -i 's/2024.ch3nyang.top/YOUR_URL/g' my-github-2024
 virtualenv venv --python=python3.12 || { echo "Failed to create virtual environment"; exit 1; }
 source venv/bin/activate || { echo "Failed to activate virtual environment"; exit 1; }
 pip3 install -r requirements.txt || { echo "Failed to install dependencies"; exit 1; }
-pip3 install gunicorn || { echo "Failed to install Gunicorn"; exit 1; }
-
-# Copy service file and start the service
-cp my-github-2024.service /etc/systemd/system
-systemctl daemon-reload || { echo "Failed to reload daemon"; exit 1; }
-systemctl start my-github-2024 || { echo "Failed to start service"; exit 1; }
-systemctl enable my-github-2024 || { echo "Failed to enable service"; exit 1; }
 
 # Obtain SSL certificate
 certbot --nginx -d YOUR_URL || { echo "Failed to obtain SSL certificate"; exit 1; }
